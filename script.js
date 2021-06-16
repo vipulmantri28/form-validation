4a7d6f98904ea3a16ae7f889d4d2f3943d7c0a93
Vipul Mantri <vipulmantri@Vipuls-MacBook-Pro.local>
2021-06-16 17:43:45 +0530

    form validation function

---
+++ script.js
@@ -0,0 +1,24 @@
+let formValidation = function(form, els) {
+    form = document.querySelector(form);
+
+    form.addEventListener ("submit", function(event) {
+        event.preventDefault();
+        els.forEach(el => {
+            el = document.querySelector(el);
+            const parent = el.parentElement;
+            const message = `<span class="error">Please enter ${el.name}.</span>`;
+
+            if (!el.value) {
+                if (!parent.innerHTML.includes(message)) {
+                    parent.innerHTML += message;
+                }
+            }else {
+                if (parent.innerHTML.includes(message)) {
+                    parent.removeChild(parent.lastChild);
+                }
+            }
+        })
+    })
+}
+
+let validate = new formValidation("#form", ["#name", "#email", "#password"])
